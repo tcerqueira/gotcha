a7f5984a903498695713453b949e2165221e1398
Tiago Cerqueira <tiago.cerqueira7777@gmail.com>
2024-09-30 17:55:09 +0100

    Fix js params

--- server/widget/src/js-params.ts
+++ server/widget/src/js-params.ts
@@ -6,11 +6,19 @@ export type JsParams = {
 
 export function getJsParams(): JsParams {
   const scripts = document.getElementsByTagName("script");
-  const currentScript = scripts[scripts.length - 1];
-  const url = new URL(currentScript.src);
+  let url;
+  for (let script of scripts) {
+    if (!script.src) continue;
+
+    let innerUrl = new URL(script.src);
+    if (innerUrl.pathname.includes("/api.js")) {
+      url = innerUrl;
+      break;
+    }
+  }
 
   const params: JsParams = {};
-  url.searchParams.forEach((value, key) => {
+  url?.searchParams.forEach((value, key) => {
     switch (key) {
       case "onload":
         params.onload = (window as any)[value];
