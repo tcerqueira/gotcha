578577f7cebc4ba56fc99aab2879c16d0977bfa0
Tiago Cerqueira <tiago.cerqueira7777@gmail.com>
2025-01-31 13:24:25 +0000

    Add preview deployment

--- infra/gotcha-server/bin/gotcha-server.ts
+++ infra/gotcha-server/bin/gotcha-server.ts
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { GotchaServerStack } from "../lib/gotcha-server-stack";
+import { GotchaApplicationStage } from "../lib/gotcha-server-stack";
 
 const app = new cdk.App();
-new GotchaServerStack(app, "GotchaServerStack", {
-  env: { account: "597088030785", region: process.env.AWS_DEFAULT_REGION },
+new GotchaApplicationStage(app, "Prod", {
+  env: {
+    account: "597088030785",
+    region: process.env.AWS_DEFAULT_REGION,
+  },
+});
+
+new GotchaApplicationStage(app, "Preview", {
+  env: {
+    account: "597088030785",
+    region: process.env.AWS_DEFAULT_REGION,
+  },
 });
--- infra/gotcha-server/lib/gotcha-server-stack.ts
+++ infra/gotcha-server/lib/gotcha-server-stack.ts
@@ -3,17 +3,34 @@ import * as apigateway from "aws-cdk-lib/aws-apigatewayv2";
 import * as logs from "aws-cdk-lib/aws-logs";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Stage, StageProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-export class GotchaServerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class GotchaApplicationStage extends Stage {
+  constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
+    new GotchaServerStack(this, "GotchaServerStack", props);
+  }
+}
+
+class GotchaServerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const stageName = Stage.of(this)?.stageName.toLowerCase() ?? "oops";
+
+    const lambdaFunction = lambda.Function.fromFunctionArn(
+      this,
+      "GotchaServer",
+      `arn:aws:lambda:${this.region}:${this.account}:function:gotcha-server-${stageName}`,
+    );
+
     const api = new apigateway.CfnApi(this, "GotchaApi", {
-      name: "gotcha-api",
+      name: `gotcha-api-${stageName}`,
       protocolType: "HTTP",
       tags: {
         organization: "gotcha",
+        environment: stageName,
       },
     });
 
@@ -23,7 +40,7 @@ export class GotchaServerStack extends cdk.Stack {
       {
         apiId: api.ref,
         integrationType: "AWS_PROXY",
-        integrationUri: `arn:aws:lambda:${this.region}:${this.account}:function:gotcha-server`,
+        integrationUri: lambdaFunction.functionArn,
         payloadFormatVersion: "2.0",
       },
     );
@@ -35,7 +52,7 @@ export class GotchaServerStack extends cdk.Stack {
     });
 
     const logGroup = new logs.LogGroup(this, "GotchaLogGroup", {
-      logGroupName: "/aws/api-gateway/gotcha-api",
+      logGroupName: `/aws/api-gateway/gotcha-api-${stageName}`,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
@@ -57,17 +74,14 @@ export class GotchaServerStack extends cdk.Stack {
       },
       tags: {
         organization: "gotcha",
+        environment: stageName,
       },
     });
 
-    const lambdaFunction = lambda.Function.fromFunctionArn(
-      this,
-      "GotchaServer",
-      `arn:aws:lambda:${this.region}:${this.account}:function:gotcha-server`,
-    );
-
-    lambdaFunction.addPermission("ApiGatewayInvoke", {
-      principal: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+    new lambda.CfnPermission(this, `ApiGatewayInvoke`, {
+      action: "lambda:InvokeFunction",
+      functionName: `gotcha-server-${stageName}`,
+      principal: "apigateway.amazonaws.com",
       sourceArn: `arn:aws:execute-api:${this.region}:${this.account}:${api.ref}/*`,
     });
 
